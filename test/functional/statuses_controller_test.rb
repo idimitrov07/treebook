5f3898ba5f7fc111f881f5720e0c6f6d77f2f815
Ivan Dimitrov <idimitrov07@gmail.com>
2013-08-27 17:44:30 +0300

    Added tests to status postings.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -17,7 +17,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should render the new page when loggen in" do
+  test "should render the new page when logged in" do
     sign_in users(:jason)
     get :new
     assert_response :success
@@ -27,10 +27,11 @@ class StatusesControllerTest < ActionController::TestCase
     post :create, status: { content: "Hello" }
     assert_response :redirect
     assert_redirected_to new_user_session_path
-  end  
+  end
 
-  test "should create status whenn logged in" do
+  test "should create status when logged in" do
     sign_in users(:jason)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -43,6 +44,12 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should get edit when logged in" do
     sign_in users(:jason)
     get :edit, id: @status
@@ -50,14 +57,14 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should redirect status update when not logged in" do
-    put :update, id: @status, status: { content: @status.content}
+    put :update, id: @status, status: { content: @status.content }
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
 
   test "should update status when logged in" do
     sign_in users(:jason)
-    put :update, id: @status, status: { content: @status.content}
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
