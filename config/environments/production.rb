ab810c4a872778b79c458730f0773ea51c4024bb
ivandimitrov07 <idimitrov07@yahoo.com>
2013-09-29 21:20:52 +0300

    edited friendships page, url option on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Treebook::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.default_url_options = { :host => 'heroku.com' }
   
   # Enable threaded mode
   # config.threadsafe!
