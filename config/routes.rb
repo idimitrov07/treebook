4e85aa16a677cf0a8f866cc3b1bc56a36b3ba549
ivandimitrov07 <idimitrov07@yahoo.com>
2013-09-29 21:43:31 +0300

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Treebook::Application.routes.draw do
     get '/logout', to: 'devise/sessions#destroy',via: :get, as: :logout
   end
 
-  devise_for :users, if Rails.env.production? :skip => [:sessions]
+  if Rails.env.production?
+    devise_for :users, :skip => [:sessions]
+  else
+    devise_for :users                    
+  end
 
   as :user do
     get "/login" => 'devise/sessions#new', as: :new_user_session
