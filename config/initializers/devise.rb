e6797f8c89602ea18344adabfe2880517cb29526
ivandimitrov07 <idimitrov07@yahoo.com>
2013-11-16 20:37:27 +0200

    precompiled assets localy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,9 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "password@example.com"
   
-  if Rails.env.production?
-    config.secret_key = '0c4e231573ecf322361475637d2892a313d16d6dabd65592c9fda734f73bd2741ff9f2c1a57f1d2eea6d3090b944e5fc2fc281e8969fb72ecec56da222b8a7ff'
-  end
+  
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
